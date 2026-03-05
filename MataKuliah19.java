00646f137122c8ce4023a294046dcf86cc924f57
“radennobioyetgit config --global user.name “radennobioyet <radensnpmb@gmail.com>
2026-03-05 20:05:24 +0700

    soal 1 praktikum 2

--- MataKuliah19.java
+++ MataKuliah19.java
@@ -4,10 +4,16 @@ public class MataKuliah19 {
     public int sks;
     public int jumlahJam;
 
+    // Constructor 1: Dengan semua parameter (seperti di gambar)
     public MataKuliah19(String kode, String nama, int sks, int jumlahJam) {
         this.kode = kode;
         this.nama = nama;
         this.sks = sks;
         this.jumlahJam = jumlahJam;
+    }
+
+    // Constructor 2: Constructor kosong (default)
+    public MataKuliah19() {
+
     }
 }
