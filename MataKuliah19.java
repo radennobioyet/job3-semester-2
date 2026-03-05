c95d60be77f12f0a589922ba6f8272e2c7eba9e2
“radennobioyetgit config --global user.name “radennobioyet <radensnpmb@gmail.com>
2026-03-05 20:04:33 +0700

    praktikum 3

---
+++ MataKuliah19.java
@@ -0,0 +1,13 @@
+public class MataKuliah19 {
+    public String kode;
+    public String nama;
+    public int sks;
+    public int jumlahJam;
+
+    public MataKuliah19(String kode, String nama, int sks, int jumlahJam) {
+        this.kode = kode;
+        this.nama = nama;
+        this.sks = sks;
+        this.jumlahJam = jumlahJam;
+    }
+}
