08968bc70ab511b5bdb207096f2b35316f0a4cd7
“radennobioyetgit config --global user.name “radennobioyet <radensnpmb@gmail.com>
2026-03-05 19:41:49 +0700

    soal 1 praktikum 2

--- Mahasiswa19.java
+++ Mahasiswa19.java
@@ -3,4 +3,13 @@ public class Mahasiswa19 {
     public String nama;
     public String kelas;
     public float ipk;
+
+    public void cetakInfo( int i ) {
+        System.out.println("Data Mahasiswa ke-" + (i + 1));
+        System.out.println("NIM   : " + this.nim);
+        System.out.println("Nama  : " + this.nama);
+        System.out.println("Kelas : " + this.kelas);
+        System.out.println("IPK   : " + this.ipk);
+        System.out.println("________________________________");
+    }
 }
